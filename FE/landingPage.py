2308f9d6c50a8a558c9cace83abf209123063667
mewrion <gaumengyew@gmail.com>
2022-03-03 20:49:53 +0800

    Edited Landing page to reflect end code

--- FE/landingPage.py
+++ FE/landingPage.py
@@ -1,20 +1,57 @@
 import tkinter as tk
+from memberFull import *
 
+## Created as template for landing page
+
+### LANDING PAGE FUNCTIONS ###
 window = tk.Tk()
 
-buttonMember = tk.Button(text = "Membership", width=25, height=5, bg="indianred")
+def openLoans():
+    window.destroy()
+    import loans
+    
+def openBooks():
+    window.destroy()
+    import books
+    
+buttonMember = tk.Button(window,
+                         text = "Membership",
+                         width=25,
+                         height=5,
+                         bg="indianred",
+                         command=membershipMenu)
 
-buttonBooks = tk.Button(text = "Books", width=25, height=5,bg="aquamarine")
+buttonBooks = tk.Button(window,
+                        text = "Books",
+                        width=25,
+                        height=5,
+                        bg="aquamarine",
+                        command=openBooks)
 
-buttonLoans = tk.Button(text = "Loans", width=25, height=5, bg="darksalmon")
+buttonLoans = tk.Button(window,
+                        text = "Loans",
+                        width=25,
+                        height=5,
+                        bg="darksalmon",
+                        command=openLoans)
 
-buttonReservation = tk.Button(text = "Reservation", width=25, height=5, bg="darkseagreen")
+buttonReservation = tk.Button(window,
+                              text = "Reservation",
+                              width=25,
+                              height=5,
+                              bg="darkseagreen")
 
-buttonFines = tk.Button(text = "Fines", width=25, height=5, bg="cadetblue")
+buttonFines = tk.Button(window,
+                        text = "Fines",
+                        width=25,
+                        height=5,
+                        bg="cadetblue")
 
-buttonReports = tk.Button(text = "Reports", width=25, height=5,bg="peru")
-
-
+buttonReports = tk.Button(window,
+                          text = "Reports",
+                          width=25,
+                          height=5,
+                          bg="peru")
 
 buttonMember.grid(row=0, column=0);
 
@@ -27,6 +64,9 @@ buttonReservation.grid(row=0, column=1);
 buttonFines.grid(row=1, column=1);
 
 buttonReports.grid(row=2, column=1);
+
+
+
 
 
 
